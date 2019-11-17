da85968e671e3f2b24b3c1a3ab908987ee295af1
Italo Tabatinga <italotabatinga@gmail.com>
2019-11-16 21:15:15 -0300

    Remote Sensor v1

--- remote_sensor/remote_sensor.py
+++ remote_sensor/remote_sensor.py
@@ -1,15 +1,15 @@
 import time
-import board
 import adafruit_dht
 from firebase import init_firebase
+import logging
 
-def init_dht_sensor(pin = board.D18):
+def init_dht_sensor(pin = 18):
   return adafruit_dht.DHT22(pin)
 
 def init_setup():
   # Define pin constants
-  ENVIRONMENT_SENSOR_PIN = board.D17
-  HIVE_SENSOR_PIN = board.D27
+  ENVIRONMENT_SENSOR_PIN = 17
+  HIVE_SENSOR_PIN = 27
 
   # Setup Firebase app
   firebase = init_firebase()
@@ -18,21 +18,51 @@ def init_setup():
   environ_sensor = init_dht_sensor(ENVIRONMENT_SENSOR_PIN)
   hive_sensor = init_dht_sensor(HIVE_SENSOR_PIN)
 
+  return firebase, environ_sensor, hive_sensor
+
+def init_logging(level=logging.INFO):
+  logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+  logger = logging.getLogger(__name__)
+  logger.setLevel(level)
+
+  return logger
+
 def main():
+  firebase, environ_sensor, hive_sensor = init_setup()
+  database = firebase.database()
+  logger = init_logging()
   while True:
       try:
-          # Print the values to the serial port
-          temperature_c = dhtDevice.temperature
-          temperature_f = temperature_c * (9 / 5) + 32
-          humidity = dhtDevice.humidity
-          print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
-                .format(temperature_f, temperature_c, humidity))
-  
+          moment = time.time()
+
+          environ_temperature = environ_sensor.temperature
+          environ_humidity = environ_sensor.humidity
+
+          database.child('environment_sensor').push({
+            "temperature": environ_temperature,
+            "humidity": environ_humidity,
+            "captured_at": moment
+          })
+          logger.debug('Captured ENVIRON: temp: {:.2f} humid: {:.2f}'.format(environ_temperature, environ_humidity))
+
+          hive_temperature = hive_sensor.temperature
+          hive_humidity = hive_sensor.humidity
+
+          database.child('hive_sensor').push({
+            "temperature": hive_temperature,
+            "humidity": hive_humidity,
+            "captured_at": moment
+          })
+          logger.debug('Captured HIVE: temp: {:.2f} humid: {:.2f}'.format(hive_temperature, hive_humidity))
+
       except RuntimeError as error:
           # Errors happen fairly often, DHT's are hard to read, just keep going
-          print(error.args[0])
+          logger.warn(error.args[0])
   
       time.sleep(2.0)
 
+def name():
+  return __name__
+
 if __name__ == '__main__':
-  pass
+  print(__name__)
